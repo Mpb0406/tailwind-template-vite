f15e249d667d178e31d0d0c4c9491695b5cf8704
Mike Bolloskis <mpb0406@gmail.com>
2023-03-02 20:52:22 -0500

    Updated theme colors and created Library directory for reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 
 
 function App() {
 
-  const themeColors = ['Primary', 'Secondary', 'Light', 'Dark', 'Mid', 'Success', 'Warning', 'Danger']
-
   return (
-    <div className='flex gap-2'>
-      {themeColors.map(color => (
-        <button className='bg-danger'>{color}</button>
-      ))}
+    <div className='m-4'>
+        <button>Submit</button>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      primary: "#4951eb",
-      secondary: "#eae618",
+      primary: "#023047",
+      secondary: "#ffb703",
       light: "#f5f5f5",
       mid: "#b7b7b7",
       dark: "#303030",
       success: "#009842",
-      warning: "#ece400",
+      warning: "#ffd500",
       danger: "#c22c2c",
     },
     extend: {},
